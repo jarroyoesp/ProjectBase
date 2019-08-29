71ba731481b70cf1c45743712ea327743375bf93
Javier Arroyo <javier.arroyo@microhealth.com>
2019-08-29 09:24:56 +0200

    Activity module added

---
+++ app/src/main/java/es/projectbase/app/di/module/ActivityModule.kt
@@ -0,0 +1,16 @@
+package es.projectbase.app.di.module
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import dagger.Module
+import dagger.Provides
+
+@Module
+abstract class ActivityModule(protected val activity: AppCompatActivity) {
+
+    @Provides
+    fun provideActivity(): AppCompatActivity = activity
+
+    @Provides
+    fun provideActiviyContext(): Context = activity.baseContext
+}
